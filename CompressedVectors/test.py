c3a290a95913e8751a45e670c48a16c39e3e56e5
Alexandr Shpergl <ashperl@gmail.com>
2017-08-28 18:13:52 +0300

    Corrected tests.

--- CompressedVectors/test.py
+++ CompressedVectors/test.py
@@ -5,16 +5,20 @@ from compressed_lists import *
 class TestScalarMultiply(unittest.TestCase):
     lst1 = [1,4,3,4,5,3,1,5]
     lst2 = [3,5,4,6,5,5]
-    lst3 = [1,2,3,'4']
+    
 
     def test_check_lst_length(self):
-        res = check_lst_len(self.lst1)
-        self.assertEqual(res, 16)
+        for lst in [self.lst1, self.lst2]:
+            res = check_lst_len(lst)
+            self.assertEqual(res, 16)
 
-    def test_check_number_is_not_int(self):
-        self.assertRaises(Exception, check_lst_len(self.lst3))
+    def test_set_next_lst(self):
+        res = set_next_lst(self.lst1, self.lst2, 0,0) 
+        self.assertEqual(res, ([1,4], [3,5], 2,2))
 
-    def test
+    def test_scalar_multiply_lst(self):
+        res = scalar_multiply_lst(self.lst1, self.lst2)
+        self.assertEqual(res, 142)
 
 if __name__ == '__main__':
     unittest.main()
